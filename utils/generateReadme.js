7b81b3f3220b400f0c67afda481832678697d899
Abby Oliva <abbyoliva6@icloud.com>
2022-05-30 01:08:11 -0600

    create license badge

--- utils/generateReadme.js
+++ utils/generateReadme.js
@@ -29,3 +29,27 @@ const createQuestions = (github, email) => {
   
     return desc + githubLink + emailLink;
 };
+
+//Create license Badge
+const createLicenseBadge = license => {
+
+    //creates badge with link of selected license
+    switch(license) {
+        case 'MIT': 
+            return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+        case 'LGPL 3.0':
+            return '[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)';
+        case 'MPL 2.0':
+            return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+        case 'AGPL 3.0':
+            return '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)';
+        case 'GPL 3.0':
+            return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+        case 'Apache 2.0':
+            return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+        case 'Unlicense':
+            return '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)';
+        default:
+            return '';
+    }
+};
