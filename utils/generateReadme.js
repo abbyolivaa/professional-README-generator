16968fa0b486823dcba8791278bdd1e8fa75c935
Abby Oliva <abbyoliva6@icloud.com>
2022-05-30 01:05:24 -0600

    table content

--- utils/generateReadme.js
+++ utils/generateReadme.js
@@ -1,4 +1,16 @@
 // Create Title and license badge
 const createTitle = (title, license) => {
     return `# ${title}\n${createLicenseBadge(license)}\n\n`;
+};
+
+// Create Table of Contents
+const createTableCont = contents => {
+    let returnStr = `<br />\n\n## Table of Contents\n\n`;
+    contents.forEach(element => {
+        if(element !== ``) {
+            returnStr += `- [${element}](#${element.toLowerCase().replace(/\s/g, '-')})\n\n`;
+        }   
+    });
+  
+    return returnStr + `\n\n`;
   };
